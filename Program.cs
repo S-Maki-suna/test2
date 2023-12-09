9036d8355a961b5ce9ae3d1d66864e081005732b
Maki Seiya <s.maki0507@gmail.com>
2023-12-09 11:33:07 +0900

    新規作成

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualBasic.CompilerServices;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// namespace Name
+// {
+//     class Program
+//     {
+//         Static void Main(String[] args)
+//         {
+            
+//         }
+//     }
+// }
